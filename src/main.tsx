056d5207d4593d34867d1aeb5a4985eaf95604ac
WillDevAc <wydemairomsilva@gmail.com>
2023-04-21 19:12:20 -0500

    review: add to global styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 
 import RouterController from './routes/routes'
 
+import './styles/global.css'
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <RouterController />
